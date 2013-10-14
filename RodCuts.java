01069d8d2b7dbd1a4fc017ab8198325857f410d9
barbie6676 <mian@lexity.com>
2013-10-14 00:09:54 -0700

    Update RodCuts.java

--- RodCuts.java
+++ RodCuts.java
@@ -7,43 +7,43 @@ return the maximum price you can get by cutting the rod into different parts.
 
 public int maxRodsPrice(int[] p, int n) {		
 		
-		int[] r = new int[n+1];
-		
-		r[0] = 0;
-		
-		for (int i = 1; i <=n; i ++) {
-			int q= Integer.MIN_VALUE;
-			int j =1;
-			for (; j <= i; j ++) {
-				q = Math.max(q,p[j-1]+maxRodsPrice(p,i-j));
-			}
-			r[i] = q;
+	int[] r = new int[n+1];
+	
+	r[0] = 0;
+	
+	for (int i = 1; i <=n; i ++) {
+		int q= Integer.MIN_VALUE;
+		int j =1;
+		for (; j <= i; j ++) {
+			q = Math.max(q,p[j-1]+maxRodsPrice(p,i-j));
 		}
-		
-		return r[n];
+		r[i] = q;
 	}
 	
-	public int memRodsPrice(int[] p, int n) {
-		int[] r = new int[n+1];
-		for (int i = 0; i < n+1 ; i ++) {
-			r[i] = Integer.MIN_VALUE;
-		}
-		
-		return memRodsPriceHelper(p,n,r);
+	return r[n];
+}
+
+public int memRodsPrice(int[] p, int n) {
+	int[] r = new int[n+1];
+	for (int i = 0; i < n+1 ; i ++) {
+		r[i] = Integer.MIN_VALUE;
 	}
 	
-	public int memRodsPriceHelper(int[] p, int n, int[] r) {
-		if (r[n] >= 0) return r[n];
-		int q;
-		if (n == 0) {
-			q = 0;
-		} else {
-			 q = Integer.MIN_VALUE;
-			for (int i = 1; i <= n ; i ++) {
-				q = Math.max(q, p[i-1]+ memRodsPriceHelper(p,n-i,r));
-			}
-			
+	return memRodsPriceHelper(p,n,r);
+}
+
+public int memRodsPriceHelper(int[] p, int n, int[] r) {
+	if (r[n] >= 0) return r[n];
+	int q;
+	if (n == 0) {
+		q = 0;
+	} else {
+		 q = Integer.MIN_VALUE;
+		for (int i = 1; i <= n ; i ++) {
+			q = Math.max(q, p[i-1]+ memRodsPriceHelper(p,n-i,r));
 		}
-		r[n] = q;
-		return q;
+		
 	}
+	r[n] = q;
+	return q;
+}
